95b9d1d0ab2f1ec89e2178023aedc776be8aa7f9
George-ongaya1git config --global user.email georgeongaya94.gmail.comgit config --golabl user.name George-ongaya1 <georgeongaya94@gmail.com>
2021-08-11 15:35:24 +0300

    added bootstrap package

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from config import config_options
 from flask_bootstrap import Bootstrap
+from config import config_options
 
 # Initializing Flask Extensions
 bootstrap = Bootstrap()
